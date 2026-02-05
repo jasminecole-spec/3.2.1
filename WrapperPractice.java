e4c725dc493b102e575eb1e858be6b42d91d20a6
Jasmine <jasmine.cole@crpusd.org>
2026-02-05 09:21:06 -0800

    added Integer

--- WrapperPractice.java
+++ WrapperPractice.java
@@ -1,7 +1,7 @@
 public class WrapperPractice
 {
-    public static void main(String[] args
+    public static void main(String[] args)
     {
-
+        Integer numberOne = new Integer(1);
     }
 }
