6ec2b498211458dde62223dc32eb351aaff549df
Jasmine <jasmine.cole@crpusd.org>
2026-02-06 08:52:56 -0800

    done

--- WrapperPractice.java
+++ WrapperPractice.java
@@ -5,5 +5,12 @@ public class WrapperPractice
         Integer numberOne = 1;
 
         numberOne = Integer.parseInt("5");
+        Double numberTwo = 2.0;
+        int x = 4;
+        numberOne.intValue();
+        numberTwo.doubleValue();
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(numberOne);
     }
 }
